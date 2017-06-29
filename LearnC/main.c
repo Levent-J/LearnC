f89afc678f09125b94452b5f61cebe55507e2812
李文靖 <levent_j@liwenjingdemacbook-pro.local>
2017-06-29 14:51:00 +0800

    Initial Commit

---
+++ LearnC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LearnC
+//
+//  Created by 李文靖 on 2017/6/29.
+//  Copyright © 2017年 李文靖. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
